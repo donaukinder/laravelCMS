3b0edd6202bc3a7d761b0031e1531a375d140f38
donaukinder <higor.carvalh94@gmail.com>
2019-12-12 18:47:38 -0400

    Auth\AdminLte

---
+++ app/Http/Controllers/Admin/Auth/LoginController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers\Admin\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
+
+class LoginController extends Controller
+{
+  
+    use AuthenticatesUsers;
+
+    /**
+     * Where to redirect users after login.
+     *
+     * @var string
+     */
+    protected $redirectTo = '/dashboard';
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('guest')->except('logout');
+    }
+
+    public function index()
+    {
+        echo "Tela de login";
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,10 @@
 <?php
 
-Route::get('/', function () {
-    return view('welcome');
+//Rotas para o site
+Route::get('/', 'Site\HomeController@index');
+
+//Rotas para o dashboard
+Route::prefix('dashboard')->group(function(){
+    Route::get('/', 'Admin\AdminController@index')->name('admin');
+    Route::get('login', 'Admin\Auth\LoginController@index')->name('login');
 });
