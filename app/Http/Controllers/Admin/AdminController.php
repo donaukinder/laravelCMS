bb8645c4f73f0eff46074298e6ab99499801d31a
donaukinder <higor.carvalho94@gmail.com>
2021-02-19 19:59:37 -0400

    alteracao na controller

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -13,6 +13,8 @@ class AdminController extends Controller
     public function __construct()
     {
         $this->middleware('auth');
+
+        dd("aqui é uma alteracao");
     }
 
     public function index(Request $request)
